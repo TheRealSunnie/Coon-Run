1d3dc6e3a8b9f3853afd084139a6ce17e4843a85
DionGrimm <31693758+DionGrimm@users.noreply.github.com>
2018-05-07 11:58:00 +0200

    Opzet voor project
    Mappen structuur en compiler

---
+++ CoonRun/dev/app.ts
@@ -0,0 +1,20 @@
+var canvas: HTMLCanvasElement;
+var ctx: CanvasRenderingContext2D; 
+
+function gameLoop() {
+    requestAnimationFrame(gameLoop);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 1280, 720);
+    ctx.beginPath();
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 5;
+    ctx.arc(400, 400, 100, 0, 2 * Math.PI);
+    ctx.stroke();
+ }
+ 
+ window.onload = () => {
+    canvas = <HTMLCanvasElement>document.getElementById('cnvs');
+    ctx = canvas.getContext("2d");
+    console.log("asf");
+    gameLoop();
+ }
