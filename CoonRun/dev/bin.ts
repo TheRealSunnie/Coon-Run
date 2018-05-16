583843cccdc92ed9119211e8ddd5213008934839
Sunnie van de Werken <sunnievandewerken@gmail.com>
2018-05-16 16:07:56 +0200

    Bin toegevoegd

---
+++ CoonRun/dev/bin.ts
@@ -0,0 +1,16 @@
+class Bin{
+
+    private x: number
+    private y: number
+    private speed: number
+
+    constructor{
+        console.log("hier komt een prullebakkie")
+        
+    }
+
+    roll(){
+
+    }
+
+}
