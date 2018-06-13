b60fb4ea2d58d83ab165ff980b6995434db6541b
Charlene de Waard <0942584@hr.nl>
2018-06-13 16:43:46 +0200

    test

--- CoonRun/dev/proverbs.ts
+++ CoonRun/dev/proverbs.ts
@@ -8,20 +8,82 @@ class Proverbs {
         },
         {
             string: "De ... in de pot vinden",
-            correct: ["appel"],
-            incorrect: ["banaan"]
+            correct: ["hond"],
+            incorrect: ["kat"]
         },
         {
             string: "Zo sluw als een ...",
-            correct: ["appel"],
-            incorrect: ["banaan"]
+            correct: ["vos"],
+            incorrect: ["vis"]
         },
         {
             string: "Als een ... in de val",
-            correct: ["appel"],
-            incorrect: ["banaan"]
+            correct: ["rat"],
+            incorrect: ["muis"]
         },
-    ]
+        {
+            string: "Hij is zo sterk als een ...",
+            correct: ["beer"],
+            incorrect: ["leeuw"]
+        },
+        {
+            string: "als de ... van huis is, dansen de muizen op tafel",
+            correct: ["kat"],
+            incorrect: ["hond"]
+        },
+    
+        /** boederij dieren */
+        /** test */ 
+        {
+            string: "Als er één ... over de dam is, volgen er meer",
+            correct: ["schaap"],
+            incorrect: ["geit"]
+        },
+        {
+            string: "Er als de ... bij zijn",
+            correct: ["kippen"],
+            incorrect: ["koeien"]
+        },
+        {
+            string: "Over ... en ... praten",
+            correct: ["koetjes en kalfjes"],
+            incorrect: ["bloemetjes en bijtjes "]
+        },
+        /** huisdieren */
+        {
+            string: "De ... uit de boom kijken",
+            correct: ["kat"],
+            incorrect: ["kip"]
+        }, 
+        {
+            string: "... bijten niet",
+            correct: ["Blaffende honden"],
+            incorrect: ["Gillende katten"]
+        },
+        {
+            string: "twee ... in één klap slaan ",
+            correct: ["vliegen"],
+            incorrect: ["mier"]
+        },
+        /** dierentuin dieren */
+        {
+            string: "...-tranen huilen",
+            correct: ["Krokodillen"],
+            incorrect: ["dinosaurus"]
+        },
+        {
+            string: "Nu komt de ... uit de mouw",
+            correct: ["aap"],
+            incorrect: ["muis"]
+        },
+        {
+            string: "Men moet de huid niet verkopen voor de ... geschoten is",
+            correct: ["beer"],
+            incorrect: ["das"]
+        },
+
+
+    ];
 
     constructor() {
 
