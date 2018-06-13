6ea1b960c304403f0bc5b2cc583726dcede730c8
DionGrimm <31693758+DionGrimm@users.noreply.github.com>
2018-06-13 15:56:11 +0200

    Merge met Jesse

---
+++ CoonRun/CoonRun/dev/app.ts
@@ -0,0 +1,58 @@
+ class Game { // Declare all the stuff
+    private canvas:HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('cnvs')
+    public ctx:CanvasRenderingContext2D = this.canvas.getContext("2d")!
+    public canvasWidth:number = 1280
+    public Spawner:Spawner
+    public levelObject: Levels
+    public currentLevel:number = 0
+    private player:Player
+    public ground:number = 720
+    public maxLifes:number = 2
+    public lifeCount:number = this.maxLifes
+    public score:number = 0
+    public dead:boolean = false
+    public startObjSpeed:number = 12
+    public objSpeed:number = this.startObjSpeed
+
+    constructor() { // Load in all the stuff
+        //console.log("new game created!")
+        this.levelObject = new Levels(this)
+        this.Spawner = new Spawner(this)
+        this.player = new Player(this)
+        // Start looping stuff
+        requestAnimationFrame(this.gameLoop);
+    }
+
+    gameLoop = ():void => {
+        // Draw setup
+        this.ctx.fillStyle = "#D3D3D3" // Color
+        this.ctx.fillRect(0, 0, 1280, 720) // Clears canvas
+
+        // Update stuff
+        this.player.update()
+        this.Spawner.update()
+        this.levelObject.update()
+
+        this.ctx.fillStyle = "black"
+        this.ctx.font = "30px Arial"
+        this.ctx.fillText(this.lifeCount + " levens", 150, 450)
+        this.ctx.fillText("Score: " + this.score + this.currentLevel, 50, 200)
+        // Proverb string
+        this.ctx.fillText(this.levelObject.currentString, this.canvasWidth/2, 200)
+        this.ctx.stroke() // This draws all of the above
+        // Next frame
+        requestAnimationFrame(this.gameLoop)
+     }
+
+
+    collision(object:Bin | Trash | Word | Life):boolean { // Checks collision between the player and a given object
+        if (object.x > this.player.x-object.width && object.x < this.player.x+this.player.width && object.y > this.player.y-object.height && object.y < this.player.y+this.player.height) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+// Makes sure stuff actually happens on load
+window.addEventListener("load", () => new Game())
---
+++ CoonRun/CoonRun/dev/word.ts
@@ -0,0 +1,48 @@
+class Word {
+
+    public width:number = 50
+    public height:number = 50
+    public x:number
+    public y:number
+    public hspeed:number
+    private game:Game
+    private index:number
+    public name:string
+    public fake:boolean = false
+    public alive:boolean = true
+
+    constructor (game:Game, index:number, fake:boolean) { 
+        this.game = game
+        this.x = this.game.canvasWidth
+        this.y = this.game.ground-this.height - 250
+        this.hspeed = this.game.objSpeed
+        this.fake = fake
+        this.index = index
+        if (this.fake) {
+            this.name = this.game.levelObject.currentProverb.incorrect[index]
+        } else {
+            this.name = this.game.levelObject.currentProverb.correct[index]
+        }
+    }
+
+    update():void {
+        this.hspeed = this.game.objSpeed
+        if (this.game.collision(this)) { 
+            this.alive = false
+            if(!this.fake) { 
+                this.game.levelObject.currentProverb.correct.splice(this.index, 1)
+                console.log(this.game.levelObject.currentProverb.correct.length);
+            } else {
+                // Loses points
+            }
+        }
+        if (this.x < 0-this.width) { 
+            this.alive = false
+        }
+        this.x -= this.hspeed
+
+        // Draw
+        if (this.fake) this.game.ctx.fillStyle = "red"; else this.game.ctx.fillStyle = "green"
+        this.game.ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
